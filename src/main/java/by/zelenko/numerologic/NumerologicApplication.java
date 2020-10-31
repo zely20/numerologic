85670a56ec8e81d576c6b99b022a368d35b340c9
zely20@mail.ru <Civik1984>
2020-10-31 11:30:05 +0300

    Initial commit

---
+++ src/main/java/by/zelenko/numerologic/NumerologicApplication.java
@@ -0,0 +1,13 @@
+package by.zelenko.numerologic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumerologicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumerologicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/zelenko/numerologic/NumerologicApplicationTests.java
@@ -0,0 +1,13 @@
+package by.zelenko.numerologic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumerologicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
