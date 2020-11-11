bc7c297fc93a8b66a2cb4bfee9d3ae0050685a50
zely20@mail.ru <Civik1984>
2020-11-11 18:04:19 +0300

    add security in memory

--- src/main/java/by/zelenko/numerologic/ui/Client/MainView.java
+++ src/main/java/by/zelenko/numerologic/ui/Client/MainView.java
@@ -4,6 +4,7 @@ import by.zelenko.numerologic.ui.Square.SquareView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -33,9 +34,11 @@ public class MainView extends AppLayout {
     private void createHeader() {
         H1 logo = new H1("Numerology");
         logo.addClassName("logo");
+        Anchor logout = new Anchor("logout", "Log out");
         Label label = new Label("Name of User");
         label.addClassName("label");
-        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, label);
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, label, logout);
+        header.expand(logo);
         header.setSpacing(true);
         header.setDefaultVerticalComponentAlignment(
                 FlexComponent.Alignment.CENTER);
